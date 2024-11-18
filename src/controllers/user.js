388688f36b3724fdf01832f88862d7c29c76b705
ojan <milyasa2468@gmail.com>
2024-11-18 16:32:27 +0700

    (refactor): refactor cookie-2

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -102,7 +102,7 @@ const Login = async (req, res) => {
     res.cookie("token", loginToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
